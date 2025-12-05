6fe235d4ad69d724fb35f7ec077a9f38f25100ec
Ujjuanku <2001ujjwaljha@gmail.com>
2025-12-05 12:28:23 +0530

    Initial commit - Spring Boot 3 + Java 21 skeleton

---
+++ spring-rag-chatbot/src/main/java/com/ujjuanku/spring_rag_chatbot/SpringRagChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.ujjuanku.spring_rag_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRagChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRagChatbotApplication.class, args);
+	}
+
+}
---
+++ spring-rag-chatbot/src/test/java/com/ujjuanku/spring_rag_chatbot/SpringRagChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ujjuanku.spring_rag_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRagChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
